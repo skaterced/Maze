bf524d11800832c08866ffc489617b432dbbbc11
skaterced <61429966+skaterced@users.noreply.github.com>
2020-10-19 17:02:55 +0200

    separated maze from 2pp

---
+++ function.h
@@ -0,0 +1,125 @@
+#ifndef function_h
+#define function_h
+
+//#include <Arduino.h>
+
+int getIndice(int x, int y){ //TO DO
+  //arduboy.fillRect(x+1,y+1,7,10,fill);
+  int temp=(x-leftBorder)/casesLength;
+  temp+=(y-upBorder)/casesHeight*casesCol;
+  //arduboy.print(temp);
+  return temp;
+}
+
+
+int drawStone(int i, bool color){ //black 0 white 1
+  int x=(i%casesCol)*casesLength+leftBorder;
+  int y=(i/casesCol)*casesHeight+upBorder;
+  arduboy.fillCircle(x,y,3,0);
+  if (color){
+    arduboy.fillCircle(x,y,2,1);
+  }
+}
+void drawStones(void){
+  for (int i=0; i<81; i++){
+    if (i==selectedI){
+      if (blink) {
+        if (BLACK_STONE==stoneArray[i]){
+          drawStone(i,0);
+        }
+        else if (WHITE_STONE==stoneArray[i]){
+          drawStone(i,1);
+        }
+      }
+    }
+    else {
+      if (BLACK_STONE==stoneArray[i]){
+      drawStone(i,0);
+      }
+      else if (WHITE_STONE==stoneArray[i]){
+        drawStone(i,1);
+      }
+    }
+  }
+}
+void SelectorManagment(void){
+    if (arduboy.justPressed(UP_BUTTON)){
+      if (p1.y>=upBorder+casesHeight){
+        p1.y-=casesHeight;
+      }
+    }
+    if (arduboy.justPressed(DOWN_BUTTON)){
+      if (p1.y<(upBorder+(casesRow-1)*casesHeight)){ 
+        p1.y+=casesHeight;
+      }
+    }
+    if (arduboy.justPressed(RIGHT_BUTTON)){
+      if (p1.x<(leftBorder+(casesCol-1)*casesLength)){
+        p1.x+=casesLength;
+      }
+    }
+    if (arduboy.justPressed(LEFT_BUTTON)){
+      if (p1.x>=(leftBorder+casesLength)){
+        p1.x-=casesLength;
+      }
+    }
+}
+void drawSelector(int i){
+
+   if (blinkTimer--==0){
+    blinkTimer=BLINK_TIMER_INIT;
+    blink=!blink;
+  }  
+  int x=(i%casesCol)*casesLength+leftBorder;
+  int y=(i/casesCol)*casesHeight+upBorder;
+  x+=adjSelectX;
+  y+=adjSelectY;
+  arduboy.drawLine(x-4,y-4,x-2,y-2,blink? 1:0);  // Why not add a variable to adjust the Selector for each game? todo?
+  arduboy.drawLine(x-4,y+4,x-2,y+2,blink? 1:0);
+  arduboy.drawLine(x+2,y+2,x+4,y+4,blink? 1:0);
+  arduboy.drawLine(x+2,y-2,x+4,y-4,blink? 1:0);
+}
+  
+void turnUpdate(void){ /////////////////////////////////////// score ///////////////////////  score ///////////////////////
+  if (1==difficulty) { // for MEMO that have bigger cards in difficulty 1
+    arduboy.fillRect(0,0,17,64,0);
+    if (p1Playing){
+      arduboy.setCursor(1,6);
+      arduboy.print(F("P1"));
+    }
+    else{
+      arduboy.setCursor(1,39);
+      arduboy.print(F("P2"));
+    }
+  }
+  else {
+    arduboy.fillRect(0,0,26,64,0);
+    if((game!=CHESS)&&(game!=GO)){ //we don't knows who's turn it is in those games
+      if (p1Playing){
+        arduboy.setCursor(1,6);
+        arduboy.print(F("P1's"));
+        arduboy.setCursor(1,14);
+        arduboy.print(F("turn"));
+      }
+      else{
+        arduboy.setCursor(1,39);
+        arduboy.print(F("P2's"));
+        arduboy.setCursor(1,47);
+        arduboy.print(F("turn"));
+      }
+    }
+  }
+  arduboy.setCursor(5,24);
+  arduboy.print(p1.score);
+  arduboy.setCursor(5,57);
+  arduboy.print(p2.score);
+  if ((GO==game)||(MILL==game)){
+    if (removing){
+      arduboy.setCursor(1,30);
+      arduboy.print(F("Removing"));
+    }
+    arduboy.drawCircle(22,27,3,1); // to picture the remaining (or placed) stones
+    arduboy.fillCircle(22,60,3,1);
+  }
+}
+#endif
---
+++ globals.h
@@ -0,0 +1,74 @@
+#ifndef globals_h
+#define globals_h
+
+#include <Arduboy2.h>
+//#include <stdlib.h>
+
+Arduboy2 arduboy;
+
+#define MENU 0
+#define MENU2 1
+#define PONG 2
+#define TRACE 3
+#define MAZE 4
+#define MEMO 5
+#define MILL 6
+#define GO 7
+#define CHESS 8
+#define REFLX 9
+
+#define BLINK_TIMER_INIT 10
+#define BLACK_STONE 1 // (P1)
+#define WHITE_STONE 2 // (P2)
+
+
+  //Controls designed for Arduboy
+bool forEmulator = false;  // ToDo : remove this and change Menu. No more need for this bool
+uint8_t P1_LEFT=UP_BUTTON;
+uint8_t  P1_RIGHT=DOWN_BUTTON;
+uint8_t  P2_RIGHT=B_BUTTON;
+uint8_t  P2_LEFT=A_BUTTON;
+
+uint8_t scoreTimer=0;
+int timer=0;
+uint8_t difficulty=2;  //must be between 1-4
+uint8_t game=MENU;
+//int temp=0;
+bool p1Playing=true; //false: p2's turn
+bool selected=false; //MILL and MEMO - when a player has selected something - ReflX True: bold arrow
+int8_t selectedI=-1;
+bool removing=false;
+bool blink=true;
+uint8_t blinkTimer=BLINK_TIMER_INIT;
+uint8_t casesLength=8;
+uint8_t casesHeight=11;
+uint8_t casesCol=7+difficulty;
+uint8_t casesRow=2+difficulty;
+int leftBorder=30;
+int upBorder=-1;
+int adjSelectX=0;
+int adjSelectY=0;
+uint8_t stoneArray [81];
+
+
+class Player {
+  public :
+    int x,y;
+    int length;
+    uint8_t dir;
+    int score;
+    Player(int X, int Y);
+};
+Player::Player(int X, int Y)
+{
+  this->x=X;
+  this->y=Y;
+  this->length=14;  //becomes "Number of Stones left" in MILL //becomes ...Weapon? in Maze
+  this->dir=0;      // in Maze, direction is managed otherwise
+  this->score=0;
+}
+
+class Player p1(4,0);
+class Player p2(122,20);
+
+#endif
